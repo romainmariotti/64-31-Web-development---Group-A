e99455c86902f877cbcb7966c1b245080aa4b954
dfain <david.faini@students.hevs.ch>
2024-12-16 14:56:00 +0100

    Jet selection + shooting sounds

--- Game/Game/game.js
+++ Game/Game/game.js
@@ -1,7 +1,41 @@
 import { CallMeteor, animateMeteors } from './meteors.js';
 import { pauseButton } from './menus.js';
 import { canvas, ctx } from './constant.js';
+import { jetActions, updateBullets } from "./jet.js";
+import { activeJet,showJetSelectionMenu } from "./selectJet.js";
+import { xwingActions, updateXwingBullets } from "./xwing.js";
 
+
+
+
+
+function animateGame() {
+    if (!gameState.paused) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBackground();
+
+        // Player and bullets
+        if (activeJet) {
+            if (activeJet.image.src.includes("X-Wing")) {
+                xwingActions();
+                updateXwingBullets();
+            } else {
+                jetActions();
+                updateBullets();
+            }
+            ctx.drawImage(activeJet.image, activeJet.x, activeJet.y, activeJet.width, activeJet.height);
+        }
+
+        // Draw meteors, score, and hearts
+        animateMeteors();
+    }
+    requestAnimationFrame(animateGame);
+}
+
+
+
+let isShootingXwing = false;
+let xwingShootingInterval = null;
 
 const background_image = new Image();
 background_image.src = "../Game/Images/sky.png";
@@ -15,21 +49,27 @@ export let gameState = {
     game_started,
 };
 
+
 //Draws the background image
 export function drawBackground(){
     ctx.drawImage(background_image, 0, 0, canvas.width, canvas.height);
 }
 
+export let preGameSelection = function () {
+    showJetSelectionMenu();
+}
+
 //Starts the game (our main)
-export let startGame= function(){
-    document.getElementById("start-screen").style.display = "none";
-    game_started = true;
+export let startGame = function () {
+    document.getElementById("start-screen").style.display = "none"; // Cache l'écran de démarrage
+    gameState.game_started = true;
     drawBackground();
     CallMeteor();
     animateMeteors();
     pauseButton();
-}
-
+    animateGame();
+    console.log("Game started!");
+};
 
 
 

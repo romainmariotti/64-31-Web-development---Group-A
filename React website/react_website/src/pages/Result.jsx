54395bf8bf271e2009fee4af18d929517f9eec17
romain.mariotti <romain.mariotti@students.hefr.ch>
2025-01-05 22:51:07 +0100

    Video of demo of the game, implements Media API

--- React website/react_website/src/pages/Result.jsx
+++ React website/react_website/src/pages/Result.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function ResultPage({ data }) {
   const resultData = data.find((page) => page.slug === "result");
+  const videoRef = useRef(null); // Reference to the video element
 
   if (!resultData) {
     return <p>Result data not found.</p>;
   }
 
+  /* Restart video through a button */
+  const restartVideo = () => {
+    if (videoRef.current) {
+      videoRef.current.currentTime = 0;
+      videoRef.current.play();
+    }
+  };
+
   return (
     <div>
       <h2>{resultData.title.rendered}</h2>
       <div dangerouslySetInnerHTML={{ __html: resultData.content.rendered }} />
+
+      {/* Video Section */}
+      <div>
+        <video
+          ref={videoRef}
+          controls
+          width="600"
+          src="/test.mp4"
+          type="video/mp4"
+        />
+        <div>
+          <button onClick={restartVideo}>Restart video</button>
+        </div>
+      </div>
     </div>
   );
 }
